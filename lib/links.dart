2e1dfdb7545b2872e89f5d845f8560ac64c6effc
waheedoofHnblaas <waheedhnblaas@gmail.com>
2023-06-25 00:01:25 -0700

    add math to add method

--- lib/links.dart
+++ lib/links.dart
@@ -1,5 +1,5 @@
 class AppLinks {
-  static const String serverLink = 'https://wwwaaahhh.000webhostapp.com/testApp/';
+  static const String serverLink = 'https://w.000webhostapp.com/testApp/';
   static const String loginLink        = '$serverLink/auth/login_teacher.php';
   static const String imageUploadLink        = '$serverLink/imagesUpload.php';
   static const String imageRemoveLink        = '$serverLink/imageRemove.php';
